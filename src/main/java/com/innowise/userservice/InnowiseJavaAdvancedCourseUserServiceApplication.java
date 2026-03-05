5cc14acd5ce1542f1be2346dc7a8acc9eb165028
tabletka97 <mtabletka.by@gmail.com>
2026-03-05 19:41:35 +0300

    init commit

---
+++ src/main/java/com/innowise/userservice/InnowiseJavaAdvancedCourseUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnowiseJavaAdvancedCourseUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnowiseJavaAdvancedCourseUserServiceApplication.class, args);
+	}
+
+}
