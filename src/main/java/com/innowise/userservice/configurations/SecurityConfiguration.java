98fbc08660ddd4f3fd4a262f41a4f3fafe988765
tabletka97 <khatkevichmatsvei@gmail.com>
2026-05-11 16:17:42 +0300

    fix: open readiness endpoint

--- src/main/java/com/innowise/userservice/configurations/SecurityConfiguration.java
+++ src/main/java/com/innowise/userservice/configurations/SecurityConfiguration.java
@@ -36,7 +36,7 @@ public class SecurityConfiguration {
                                 )
                 )
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/actuator/health/liveness").permitAll()
+                        .requestMatchers("/actuator/health/*").permitAll()
                         .requestMatchers("/users/**", "/cards/**").authenticated()
                         .requestMatchers("/**").hasRole("ADMIN"))
                 .build();
